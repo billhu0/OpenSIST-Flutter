8d87b726a7b701821e2979872dba01c1850ea11f
billhu0 <shhuluojia@163.com>
2025-07-20 12:23:17 -0400

    updated icons for android and ios

--- build-unsigned-ipa.sh
+++ build-unsigned-ipa.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-flutter build ios --release --no-codesign
 rm -rf Payload
+rm -rf build
+rm -f "OpenSIST-beta-unsigned.ipa"
+flutter build ios --release --no-codesign
 cp -r build/ios/iphoneos/Runner.app Payload
-zip -r "OpenSIST-beta-unsigned.ipa" Payload
+zip -r MyApp.ipa Payload
